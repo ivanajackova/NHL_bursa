c67cce2dec064a222d02a66763376580f9532636
ivulinajackovam <ivulinajackova@gmail.com>
2025-06-11 00:08:33 +0200

    Přidání inbox_view a oprava routing

--- nhl_bursa/messaging/urls.py
+++ nhl_bursa/messaging/urls.py
@@ -3,3 +3,4 @@ from . import views
 
 urlpatterns = [
     path('inbox/', views.inbox_view, name='inbox'),
+]
--- nhl_bursa/messaging/views.py
+++ nhl_bursa/messaging/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 
 
-def inbox_view(request):
-    return render(request, 'messaging/inbox.html')
-
-
 from django.shortcuts import render
 
+def inbox_view(request):
+    return render(request, 'messaging/inbox.html')
 # Create your views here.
