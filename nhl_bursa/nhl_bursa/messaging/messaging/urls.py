dc4b24043953fcc1a41428b777add318b625166a
ivulinajackovam <ivulinajackova@gmail.com>
2025-06-12 00:37:49 +0200

    Odstranění duplicitních složek messaging, marketplace a nhl_bursa

--- nhl_bursa/nhl_bursa/messaging/messaging/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('inbox/', views.inbox_view, name='inbox'),
-]
--- nhl_bursa/nhl_bursa/messaging/messaging/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def inbox_view(request):
-    return render(request,'messaging/inbox.html')
-# Create your views here.
-def message_inbox_view():
-    return None
