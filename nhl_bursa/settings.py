d8b0de9b9e9379483deac46a10992949bb3397af
ivulinajackovam <ivulinajackova@gmail.com>
2025-06-11 22:54:21 +0200

    Vyčištění projektu – odstranění nepotřebných složek

--- nhl_bursa/settings.py
+++ nhl_bursa/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
     'albums',
     'users',
     'marketplace',
-    'messaging',
+    'nhl_bursa.messaging',
 
 ]
 
@@ -124,6 +124,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- nhl_bursa/urls.py
+++ nhl_bursa/urls.py
@@ -1,6 +1,5 @@
 """
 URL configuration for nhl_bursa project.
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
@@ -14,18 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('albums.urls')),
-    path('messaging/', include('messaging.urls')),
+    path('messaging/', include('nhl_bursa.messaging.urls')),
     # TOTO PŘIDEJ
 ]
 
