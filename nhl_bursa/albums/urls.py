39760d5c2fdb61d833d55a1ac01695d9a460ca8b
ivulinajackovam <ivulinajackova@gmail.com>
2025-06-12 15:05:29 +0200

    Fix: opravený include v albums/urls.py na nhl_bursa.albums.urls

--- nhl_bursa/albums/urls.py
+++ nhl_bursa/albums/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('albums.urls')),
+    path('', include('nhl_bursa.albums.urls')),
 ]
 
 if settings.DEBUG:
