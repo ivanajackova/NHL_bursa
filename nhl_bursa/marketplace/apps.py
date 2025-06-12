88b9823538a9dcae3b0d3800d75124331d9f1a1c
ivulinajackovam <ivulinajackova@gmail.com>
2025-06-12 14:05:33 +0200

    Doplněny apps.py soubory do všech aplikací

--- nhl_bursa/marketplace/apps.py
+++ nhl_bursa/marketplace/apps.py
@@ -1,12 +1,5 @@
 from django.apps import AppConfig
 
-<<<<<<< HEAD
 class MarketplaceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'marketplace'
-=======
-
-class MarketplaceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'marketplace'
->>>>>>> d164f05 (akouška save)
+    name = 'nhl_bursa.marketplace'
