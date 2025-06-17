718d8bef90be18a6bdd992b83b34467b54062137
ivulinajackovam <ivulinajackova@gmail.com>
2025-06-17 21:38:12 +0200

    Funkční verze 17.6.2025, kdyby neco vrát se sem

--- nhl_bursa/albums/views.py
+++ nhl_bursa/albums/views.py
@@ -6,3 +6,4 @@ def album_list(request):
     return render(request, 'albums/album_list.html', {'cards': cards})
 
 #Definuje funkci (view), která zpracovává HTTP požadavek.Django ji zavolá, když uživatel navštíví určitou URL, například /albums/.
+#tady ukládám fungujívi verzi na it
