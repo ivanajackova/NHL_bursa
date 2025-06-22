d336ee35c56a290f50d3d81746e4811f0b33fdf5
ivulinajackovam <ivulinajackova@gmail.com>
2025-06-22 16:07:37 +0200

    Přidání funkce pod kartu,uprav popis, vymaž kartu, pridej na bursu

--- nhl_bursa/albums/urls.py
+++ nhl_bursa/albums/urls.py
@@ -7,4 +7,7 @@ from . import views
 urlpatterns = [
     path('', views.album_list, name='album_list'),
     path('add/', views.add_card, name='add_card'),
+    path('edit/<int:card_id>/', views.edit_card, name='edit_card'),
+    path('delete/<int:card_id>/', views.delete_card, name='delete_card'),
+    path('toggle_sale/<int:card_id>/', views.toggle_sale, name='toggle_sale'),
 ]
--- nhl_bursa/albums/views.py
+++ nhl_bursa/albums/views.py
@@ -17,6 +17,29 @@ def add_card(request):
             card.owner = request.user
             card.save()
             return redirect('album_list')
+
+    def edit_card(request, card_id):
+        card = get_object_or_404(Card, id=card_id, owner=request.user)
+        if request.method == 'POST':
+            form = CardForm(request.POST, request.FILES, instance=card)
+            if form.is_valid():
+                form.save()
+                return redirect('album_list')
+        else:
+            form = CardForm(instance=card)
+        return render(request, 'albums/add_card.html', {'form': form})
+
+    def delete_card(request, card_id):
+        card = get_object_or_404(Card, id=card_id, owner=request.user)
+        card.delete()
+        return redirect('album_list')
+
+    def toggle_sale(request, card_id):
+        card = get_object_or_404(Card, id=card_id, owner=request.user)
+        card.is_for_sale = not card.is_for_sale
+        card.save()
+        return redirect('album_list')
+
     else:
         form = CardForm()
     return render(request, 'albums/add_card.html', {'form': form})
